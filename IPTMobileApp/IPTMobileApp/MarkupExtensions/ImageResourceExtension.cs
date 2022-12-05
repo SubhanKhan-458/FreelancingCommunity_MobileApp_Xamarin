e9790a24482781c899f68a7b9f7304200b9d7413
SubhanKhan-458 <codephobia458@gmail.com>
2022-12-05 22:04:21 +0500

    added.

---
+++ IPTMobileApp/IPTMobileApp/MarkupExtensions/ImageResourceExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IPTMobileApp.MarkupExtensions
+{
+    [ContentProperty(nameof(ResourceId))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string ResourceId { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrWhiteSpace(ResourceId))
+            {
+                return null;
+            }
+
+            return ImageSource.FromResource(ResourceId);
+
+        }
+    }
+}
